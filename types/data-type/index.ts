fc8ee36e5e3384f373587555e7a74398222c3b9a
Frank Sandqvist <kankki@gmail.com>
2020-10-19 12:57:41 +0300

    Add data type types

---
+++ types/data-type/index.ts
@@ -0,0 +1,21 @@
+export type BooleanDataType = boolean;
+
+export type NumberDataType = number;
+
+export type TextDataType = string;
+
+export type FileAnyDataType = {
+  file: string;
+  originalFilename?: string;
+};
+
+export type FileImageDataType = {
+  image: string;
+  originalFilename?: string;
+};
+
+export type FileSVGDataType = {
+  svg: string;
+  originalFilename?: string;
+  unit: 'mm' | 'pt' | 'inches';
+};
